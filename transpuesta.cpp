bcebfdc256bc63feddcc32ea765dfd7230c79496
Valeria <vmaqueraj@unjbg.edu.pe>
2026-07-08 10:07:30 -0500

    Version final, salida de la transpuesta

--- transpuesta.cpp
+++ transpuesta.cpp
@@ -9,11 +9,17 @@ int main (){
 	int M[n][m];
 	
 	for (int i=0; i<n; i++){
-		for (int j=0; j<n; j++){
-			cin >> M[n][m];
+		for (int j=0; j<m; j++){
+			cin >> M[i][j];
 		}
 	}
 	
+	for (int i=0; i<m; i++){
+		for (int j=0; j<n; j++){
+			cout << M[j][i] << " ";
+		}
+		cout << endl;
+	}
 	
 	return 0;
 }

d96904a03fd24e6c0fdfe87934d6cdf62b8cd92a
Valeria <vmaqueraj@unjbg.edu.pe>
2026-07-08 09:34:14 -0500

    Identificar cada elemento de la matriz

--- identidad.cpp
+++ identidad.cpp
@@ -4,7 +4,21 @@
 using namespace std;
 
 int main () {
+	int n;
+	cin >> n;
+	int Matriz[n][n];
 	
+	
+	for (int i=0; i<n; i++){
+		for (int j=0; j<n; j++){
+			if (i==j) {
+				Matriz[i][j] = 1;
+			}
+			else{
+				Matriz[i][j] = 0;
+			}
+		}
+	}
 	
 	
 	

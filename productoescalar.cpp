59b22f062faa6aff4b6f02ca289366cd74268d7e
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-08 23:30:47 -0500

    Version final, calculo de la nueva matriz e impresion de ella

--- productoescalar.cpp
+++ productoescalar.cpp
@@ -7,6 +7,7 @@ int main () {
 	int k, m, n;
 	cin >> k >> m >> n;
 	int A[m][n];
+	int P[m][n];
 	
 	for (int i=0; i<m; i++){
 		for (int j=0; j<n; j++){
@@ -14,6 +15,20 @@ int main () {
 		}
 	}
 	
+	for (int i=0; i<m; i++){
+		for (int j=0; j<n; j++){
+			P[i][j] = k * A[i][j];
+		}
+	}	
+	
+	cout << endl;
+	
+	for (int i=0; i<m; i++){
+		for (int j=0; j<n; j++){
+			cout << P[i][j] << " ";
+		}
+		cout << endl;
+	}		
 	
 	
 	return 0;

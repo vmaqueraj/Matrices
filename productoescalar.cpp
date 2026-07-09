dfaabf2e141fcd2073eade544a0e3fcea66b1a0b
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-08 23:25:45 -0500

    Estructura inicial

---
+++ productoescalar.cpp
@@ -0,0 +1,16 @@
+// Producto de un escalar por una matriz
+
+#include <iostream>
+using namespace std;
+
+int main () {
+	int k, m, n;
+	cin >> k >> m >> n;
+	int A[m][n];
+	
+	
+	
+	
+	
+	return 0;
+}

8d1809fd2d32701672e2dd53175386fb0da42550
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-08 22:26:32 -0500

    Version final, leer matriz e imprimir la suma

--- sumatcuadrada.cpp
+++ sumatcuadrada.cpp
@@ -7,11 +7,16 @@ int main () {
 	int n, s;
 	s = 0;
 	cin >> n;
-	int A[n];
+	int A[n][n];
 	
+	for (int i=0; i<n; i++){
+		for (int j=0; j<n; j++){
+			cin >> A[i][j];
+			s = s + A[i][j];
+		}
+	}
 	
-	
-	
-	
+	cout << s;
+		
 	return 0;
 }

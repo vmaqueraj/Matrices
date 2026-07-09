08604b3d8476086f854b1ddd70a627675deb4db8
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-08 23:07:32 -0500

    Estructura inicial

---
+++ sumdiagonal.cpp
@@ -0,0 +1,16 @@
+// Suma de elementos de la diagonal de una matriz cuadrada
+
+#include <iostream>
+using namespace std;
+
+int main () {
+	int n, s;
+	s = 0;
+	cin >> n;
+	int A[n][n];
+	
+	
+	
+	
+	return 0;
+}

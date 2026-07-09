e5d95e3e7ff4490a2d4d9812770d3b6662ca4f08
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-08 23:19:08 -0500

    Lectura de matrices A y B

--- sumamatrices.cpp
+++ sumamatrices.cpp
@@ -7,6 +7,20 @@ int main () {
 	int m, n;
 	cin >> m >> n;
 	int A[m][n];
+	int B[m][n];
+	
+	for (int i=0; i<m; i++){
+		for (int j=0; j<n; j++){
+			cin >> A[i][j];
+		}
+	}
+	
+	for (int i=0; i<m; i++){
+		for (int j=0; j<n; j++){
+			cin >> B[i][j];
+		}
+	}	
+	
 	
 	
 	

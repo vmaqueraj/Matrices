30ae9ebf487349eec249df71bb148cb6136667a9
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-09 20:36:33 -0500

    Hallar cada término de la matriz C

--- productoMatrices.cpp
+++ productoMatrices.cpp
@@ -25,6 +25,17 @@ int main () {
 		}
 	}
 	
+	// Elaborar matriz C
+	for (int i=0; i<m; i++){
+		for (int j=0; j<p; j++){
+			C[i][j] = 0;
+			for (int k=0; k<n; k++){
+				C[i][j] = C[i][j] + A[i][k] * B[k][j];
+			}
+		}
+	}
+	
+	
 	
 	return 0;
 }

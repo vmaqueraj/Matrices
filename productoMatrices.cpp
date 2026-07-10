cbdaa2ee1dd0f87a43ebaaee3ea1988ea6142e30
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-09 20:39:45 -0500

    Version final, imprimir la matriz C

--- productoMatrices.cpp
+++ productoMatrices.cpp
@@ -35,7 +35,15 @@ int main () {
 		}
 	}
 	
+	// Escribir la matriz C
+	cout << endl;
 	
+	for (int i=0; i<m; i++){
+		for (int j=0; j<p; j++){
+			cout << C[i][j] << " ";
+		}
+		cout << endl;
+	}
 	
 	return 0;
 }
